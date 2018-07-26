5a1033dd06b1d4f27d1ff057537dab00c8642464
Rance Zawada <razawada@gmail.com>
2018-07-26 14:10:32 -0400

    Fix small comment typo

--- sudoku.rb
+++ sudoku.rb
@@ -13,6 +13,7 @@ class SudokuGame
   def get_pos
     pos = nil
     # raise "This is the easiest error you're gonna get, chief.  It only gets harder going forward.  Remove me (or comment me out), re-run the code to make sure it works, and go on to the next level"
+    # this is the version after we'vve set up git names
 
     until pos && valid_pos?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
